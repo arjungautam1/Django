602000521719baed1be017436f901be489a8eb92
Laserarjun876 <laserarjun876@gmail.com>
2020-01-05 12:37:38 +0545

    Passing Dyanmic datas and photos

---
+++ Static/travello/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Destination
+
+# Create your views here.
+def index(request):
+
+    dest1=Destination()
+    dest1.name='Taplejung'
+    dest1.desc='This is an amazing city to  live .'
+    dest1.img='destination_1.jpg'
+    dest1.price=700
+
+    dest2=Destination()
+    dest2.name='Kathmandu'
+    dest2.desc='Most dirtiest city'
+    dest2.img='destination_2.jpg'
+    dest2.price=200
+
+    dest3=Destination()
+    dest3.name='Chitwan'
+    dest3.desc='This city has a National Park'
+    dest3.img='destination_3.jpg'
+    dest3.price=600
+
+    dests=[dest1,dest2,dest3]
+
+    return render(request,"index.html",{'dests':dests})
