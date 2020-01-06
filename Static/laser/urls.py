146d31c33787bb0e4753043261e2b2037167b721
Laserarjun <laserarjun876@gmail.com>
2020-01-06 17:44:49 +0545

    Static media

--- Static/laser/urls.py
+++ Static/laser/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
 
     path('',include('travello.urls')),
     url(r'^admin/', admin.site.urls),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
